1989f6ee740a022df9c4426366c106e428c4d773
Natheem Scott <st10109685@vcconnect.edu.za>
2024-03-22 20:57:38 +0100

    First Commit

---
+++ NatheemScott_ST10109685_CLDVPOE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NatheemScott_ST10109685_CLDVPOE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
